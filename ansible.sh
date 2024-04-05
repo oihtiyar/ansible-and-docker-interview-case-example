c9b0c3a822c6064287afc7352791a995c3c27fa6
oihtiyar <50960588+oihtiyar@users.noreply.github.com>
2024-04-05 16:57:49 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+# install ansible
 sudo yum update -y
 sudo yum install -y yum-utils
 sudo yum install ansible -y
+
+# install docker
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
@@ -12,10 +17,26 @@ sudo pip3 install docker
 sudo pip3 install pymongo
 sudo pip3 install requests
 sudo pip3 install docker-py
-sudo docker build -t oimon .
 ansible-galaxy collection install community.mongodb -y
+
+
+# then install terraform // optional
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+
+# create case files & update configs
 mkdir case-a
 mkdir case-b
 sudo vi ~/.bashrc // buraya dosya içeriğini girmeliyiz tee ile veya echo ile olabilir
 
+alias node1='docker exec -it node1 /bin/bash'
+alias node2='docker exec -it node2 /bin/bash'
+alias node3='docker exec -it node3 /bin/bash'
+
 source ~/.bashrc
+
+# build docker image // optional
+
+# sudo docker build -t oimon .
