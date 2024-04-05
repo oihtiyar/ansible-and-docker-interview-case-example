fccdf92868c7235f22c303f387d8b843d3bd2262
oihtiyar <50960588+oihtiyar@users.noreply.github.com>
2024-04-05 16:42:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "ec2" {
   source                 = "./modules/ec2"
   instance_ami_name      = "ami-0b26b56ad6eda399a"
   instance_type          = "t2.medium"
-  key_name               = "oi-ansible-KP"
-  vpc_security_group_ids = ["sg-04b5c13a54751163a"]
-  subnet_id              = "subnet-0c3d9653cffbf3e59"
+  key_name               = "YOUR-KEY-PAIR"
+  vpc_security_group_ids = ["YOUR-SG-ID"]
+  subnet_id              = "YOUR-SUBNET-ID"
 }
